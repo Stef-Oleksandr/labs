bcfde0315dee0ffe4d676c25d86f1897f5f72d0f
Stef-Oleksandr <sanekstefanskiy@gmail.com>
2024-09-11 18:52:50 +0300

    Add files via upload
    lr1

---
+++ lr1_1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        System.out.printf("\nThis task done by Stefanskiy Oleksandr PZ-21-1/9!");
+            }
+}
