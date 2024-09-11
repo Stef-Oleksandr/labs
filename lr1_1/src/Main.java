835d4158801ae178c7ceac76b06728f2110b45b2
Stef-Oleksandr <sanekstefanskiy@gmail.com>
2024-09-11 18:56:51 +0300

    Add files via upload
    lr1(new)

--- lr1_1/src/Main.java
+++ lr1_1/src/Main.java
@@ -1,6 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         System.out.printf("\nThis task done by Stefanskiy Oleksandr PZ-21-1/9!");
+        LocalDate currentDate= LocalDate.now();
+        System.out.printf("\nToday's date is %s.", currentDate);
             }
 }
