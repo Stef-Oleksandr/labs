b1c1bd026c5cd407d4c56f77cc4cce5d68b17e11
Stef-Oleksandr <sanekstefanskiy@gmail.com>
2024-10-03 17:19:27 +0300

    1

---
+++ lr3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\uD83D\uDE02"); // 😂 Сміх до сліз
+        System.out.println("\uD83D\uDE0D"); // 😍 Закоханий смайлик
+        System.out.println("\uD83D\uDE80"); // 🚀 Ракета
+    }
+}
